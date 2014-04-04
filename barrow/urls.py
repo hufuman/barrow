9ad1b550342dac70cb59ddee4ff88b313323cba5
Jack River <ritksm@gmail.com>
2014-04-04 11:21:15 +0800

    fix url patterns

--- barrow/urls.py
+++ barrow/urls.py
@@ -4,10 +4,11 @@
 __author__ = 'Jack River'
 
 from django.conf.urls import patterns, include, url
-from barrow.views import FetchUnreadResultView, IndexView, FetchByTimestampView
+from barrow.views import *
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'fetch/(?P<application>\w+)/unread', FetchUnreadResultView.as_view(), name='fetch-unread'),
     url(r'fetch/(?P<application>\w+)/time/(?P<timestamp>\w+)', FetchByTimestampView.as_view(), name='fetch-by-time'),
+    url(r'reset_spider/$', ResetSpiderView.as_view(), name='reset-spider'),
 )
